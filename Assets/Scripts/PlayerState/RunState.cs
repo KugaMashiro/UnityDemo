e21e7b374ab41f8da5ab3439710d7ef712f11d0b
Mashiro <kugamashiro@qq.com>
2025-08-14 18:58:54 +0800

    add run state

--- Assets/Scripts/PlayerState/RunState.cs
+++ Assets/Scripts/PlayerState/RunState.cs
@@ -1,52 +1,71 @@
+using System;
 using UnityEngine;
 
-public class RunState: IPlayerState 
+public class RunState : IPlayerState
 {
-    private PlayerStateManager stateManger;
-    private Vector2 movementInput;
+    private readonly PlayerStateManager _stateManger;
+    private readonly Action<MovementInputEventArgs> _onMovementInput;
+    private readonly Action _onRunButtonPressed;
 
-    public RunState(PlayerStateManager manager) 
+    private Vector2 _cachedMovement;
+    public RunState(PlayerStateManager manager)
     {
-        stateManger = manager;
+        _stateManger = manager;
+        _onMovementInput = OnMovementInput;
+        _onRunButtonPressed = OnRunButtunPressed;
     }
+
 
     public void Enter()
     {
+        EventCenter.OnMovementInput += _onMovementInput;
+        EventCenter.OnRunButtunPressed += _onRunButtonPressed;
 
+        _cachedMovement = _stateManger.movementInput;
+        float clampInput = 0.9f;//Mathf.Clamp(_cachedMovement.magnitude, 0.7f, 0.9f);
+        _stateManger.AnimSmoothTransition(AnimParams.MoveState, clampInput, 0.1f);
     }
 
-    public void Update() 
+    public void Update()
     {
 
     }
 
-    public void Exit() 
+    public void Exit()
     {
-
+        EventCenter.OnMovementInput -= _onMovementInput;
+        EventCenter.OnRunButtunPressed -= _onRunButtonPressed;
     }
-
-    public void HandleRoll() 
-    {
-
-    }
-
-    public void HandleAttack()
-    {
-
-    }
-
-    public void HandleHit() 
-    {
-
-    }
-
-    public void HandleMovement()
-    {
-        
-    }
-
     public void FixedUpdate()
     {
-        //throw new System.NotImplementedException();
+        HandleMovement();
+    }
+
+    private void OnMovementInput(MovementInputEventArgs e)
+    {
+        _cachedMovement = e.Movement;
+        if (!e.HasMovement)
+        {
+            EventCenter.PublishStateChange(PlayerStateType.Idle);
+        }
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 moveDir = _stateManger.GetCameraRelMoveDir(_cachedMovement, Camera.main.transform);
+        //Debug.Log(string.Format("in walk state, moveDir = {0}", moveDir));
+        // if (!MoveDirUtils.IsValidMoveDirection(moveDir))
+        //     return;
+        // if (moveDir.sqrMagnitude < 0.01f)
+        //         return;
+
+        _stateManger.Controller.Face(moveDir);
+
+        _stateManger.Controller.Move(moveDir, _stateManger.Status.RunSpeed, Time.fixedDeltaTime);
+    }
+
+    private void OnRunButtunPressed()
+    {
+        EventCenter.PublishStateChange(PlayerStateType.Walk);
     }
 }
